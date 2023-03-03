dc169ec9f605188dc12b9b266f06c15a9b41fe60
Sau1707 <83017316+Sau1707@users.noreply.github.com>
2023-03-03 19:26:19 +0100

    Add header to script

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,71 @@
 const gulp = require('gulp');
+const insert = require('gulp-insert');
 const fs = require('fs');
 const concat = require('gulp-concat');
 var cleanCSS = require('gulp-clean-css');
 const through2 = require('through2');
 
 OUTPUT = 'merged.user.js';
+HEADER = `
+// ==UserScript==
+// @name         ModernBot
+// @author       Sau1707
+// @description  A modern grepolis bot
+// @version      1.0.0
+// @match        http://*.grepolis.com/game/*
+// @match        https://*.grepolis.com/game/*
+// ==/UserScript==
 
+`
 // Gulp task to minify JavaScript files
 gulp.task('scripts', () => {
-	return gulp
-		.src('./src/util.js')
-		.pipe(concat(OUTPUT))
-		.pipe(gulp.src(['./src/**/*.js', '!./src/setup.js', '!./src/util.js']))
-		.pipe(concat(OUTPUT))
-		.pipe(gulp.src('./src/setup.js'))
-		.pipe(concat(OUTPUT))
-		.pipe(gulp.dest('./dist/'));
+    return gulp.src('./src/util.js')
+        .pipe(concat(OUTPUT))
+        .pipe(gulp.src(['./src/**/*.js', '!./src/setup.js', '!./src/util.js']))
+        .pipe(concat(OUTPUT))
+        .pipe(gulp.src('./src/setup.js'))
+        .pipe(concat(OUTPUT))
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('styles', function () {
-	return gulp
-		.src('./styles/**/*.css')
-		.pipe(concat('all.css'))
-		.pipe(cleanCSS())
-		.pipe(gulp.dest('./dist/'));
+    return gulp
+        .src('./styles/**/*.css')
+        .pipe(concat('all.css'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('merge', function () {
-	let cssContents = fs.readFileSync('./dist/all.css', 'utf8');
+    let cssContents = fs.readFileSync('./dist/all.css', 'utf8');
 
-	return gulp
-		.src(`./dist/${OUTPUT}`)
-		.pipe(
-			through2.obj(function (file, enc, cb) {
-				file.contents = Buffer.concat([
-					Buffer.from('var style = document.createElement("style");\n'),
-					Buffer.from('style.textContent = `' + cssContents + '`;\n'),
-					Buffer.from('document.head.appendChild(style);\n'),
-					Buffer.from('\n'),
-					file.contents,
-				]);
-				cb(null, file);
-			}),
-		)
-		.pipe(gulp.dest('./dist/'));
+    return gulp
+        .src(`./dist/${OUTPUT}`)
+        .pipe(
+            through2.obj(function (file, enc, cb) {
+                file.contents = Buffer.concat([
+                    Buffer.from('var style = document.createElement("style");\n'),
+                    Buffer.from('style.textContent = `' + cssContents + '`;\n'),
+                    Buffer.from('document.head.appendChild(style);\n'),
+                    Buffer.from('\n'),
+                    file.contents,
+                ]);
+                cb(null, file);
+            }),
+        )
+        .pipe(insert.prepend(HEADER))
+        .pipe(gulp.dest('./dist/'));
 });
 
 // Gulp task to minify all files
 gulp.task('default', (cb) => {
-	gulp.watch('./src/**/*.js', gulp.series('scripts', 'styles', 'merge'));
-	gulp.watch('./styles/**/*.css', gulp.series('scripts', 'styles', 'merge'));
+    gulp.watch('./src/**/*.js', gulp.series('scripts', 'styles', 'merge'));
+    gulp.watch('./styles/**/*.css', gulp.series('scripts', 'styles', 'merge'));
 
-	gulp.series('scripts', 'styles', 'merge')(cb);
+    gulp.series('scripts', 'styles', 'merge')(cb);
 
-	process.on('SIGINT', () => {
-		console.log('Exiting...');
-		process.exit(0);
-	});
+    process.on('SIGINT', () => {
+        console.log('Exiting...');
+        process.exit(0);
+    });
 });
